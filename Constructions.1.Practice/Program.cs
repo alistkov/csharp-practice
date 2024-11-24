a1e7bdedafb304f260b9ab7de2a58ec545f32f7a
Anton Listkov <antonlistkov@gmail.com>
2024-11-24 12:49:57 +0100

    constructions 1 task

---
+++ Constructions.1.Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = new int [10];
+
+for (var i = 0; i < numbers.Length; i += 1)
+{
+    numbers[i] = new Random().Next(-50, 50);
+}
+
+foreach (var number in numbers)
+{
+    if (number > 0)
+        Console.WriteLine(number);
+}
