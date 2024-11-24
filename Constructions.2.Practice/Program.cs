cf150d220640b61eedecee28102055aa37b2201e
Anton Listkov <antonlistkov@gmail.com>
2024-11-24 12:53:12 +0100

    constructions 2 task

---
+++ Constructions.2.Practice/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] words = { "word", "", "not empty string", "another string", "true", "", "", "last" };
+
+foreach (var word in words)
+{
+    if (word.Length > 0)
+        Console.WriteLine(word);
+}
