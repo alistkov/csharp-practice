94c4b1780b7731b3e53bde1c92241ea56409ce9c
Anton Listkov <antonlistkov@gmail.com>
2024-11-25 13:49:19 +0100

    remove unnecessary code

--- Methods.1.Practice/Program.cs
+++ Methods.1.Practice/Program.cs
@@ -1,8 +1,7 @@
 ﻿List<int> GetAboveZero(int[] numbers)
 {
-    // Какой вариант лучше использовать?
     var result = new List<int>();
-    // List<int> result = [];
+    
     foreach (var number in numbers)
     {
         Console.WriteLine(number);
