4a75ea66970960d2a4edebbec67a1fc197f75b2e
Anton Listkov <antonlistkov@gmail.com>
2024-11-29 13:20:32 +0100

    collections 2 task

---
+++ Collections.2.Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Collections._2.Practice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    
+    public static string[] GetWordsWithEvenKeys(Dictionary<int, string> dictionary)
+    {
+        var words = new List<string>();
+
+        foreach (var pair in dictionary)
+        {
+            if (pair.Key % 2 == 0)
+                words.Add(pair.Value);
+        }
+
+        return words.ToArray();
+    }
+}
